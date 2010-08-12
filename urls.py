5d78d16097144ab3939a9ea06ab79143e802542a
joshlory <loryjn@missouri.edu>
2010-08-12 02:08:53 +0000

    Initial import.
    git-svn-id: file:///Users/Josh/Desktop/loryjn_unfuddle/svn/pspmizzou@1 4feed158-5ac6-4983-99f9-447a166aa650

---
+++ urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls.defaults import *
+from django.contrib import admin
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+
+    (r'^/events' include('pspmizzou.events.urls')),
+    (r'^/committees' include('pspmizzou.committees.urls')),
+    (r'^/brothers' include('pspmizzou.brothers.urls')),
+
+    (r'^admin/', include(admin.site.urls)),
+)
